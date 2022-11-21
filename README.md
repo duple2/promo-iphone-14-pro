258fbc59a565d4b227fcb2c166c43e83aefa62aa
Амин Рахимов <duple9923@gmail.com>
2022-11-21 20:45:25 +0300

    Readme and screenshots are added

---
+++ README.md
@@ -0,0 +1,31 @@
+# IPhone 14 promo landing
+
+An adaptive landing introducing IPhone 14 lineup and other gadgets. The site is supported by Firebase Realtime database , where all the gadgets-concerning data is stored. The following features are developed in this web-site:
+
+## Smooth scrolling using links in the header
+
+Click on any link and smoothly reach the section you would like to skip to
+
+## Active tabs and shipment form
+
+Click on the colour in the IPhones lineup and reveal all possible appearances of the smartphone. Each tab contains the opportunity to open the form and fill the shipment info to 'fast purchase'
+
+![This is an image](./screenshots/site1.jpg)
+
+![This is an image](./screenshots/site2.jpg)
+
+![This is an image](./screenshots/site3.jpg)
+
+## Tech characteristics in an accordion dropdowns
+
+Reveal the smartphone characteristics that are grouped in the accordion dropdowns
+
+![This is an image](./screenshots/site4.jpg)
+
+## Other available gadgets
+
+The section provides the gadgets that are also available in the store. Gadgets-concerning data is fetched from the Realtime Database and rendered in the section. Click more to reveal see more products
+
+![This is an image](./screenshots/site5.jpg)
+
+## To launch the web site upload the folder and open index.html
--- scripts/accordeon.js
+++ scripts/accordeon.js
@@ -1,20 +1,20 @@
-const accordeonFunc = () => {
-  const chItems = document.querySelectorAll(".characteristics__item");
-  chItems.forEach((item) => {
-    const chButton = item.querySelector(".characteristics__title");
+const accordeonFunc = () => { /* создаем функцию  */
+  const chItems = document.querySelectorAll(".characteristics__item"); /* указываем с каким файлом будем работать */
+  chItems.forEach((item) => {  
+    const chButton = item.querySelector(".characteristics__title"); /* указываем с какими данными работаем */
     const chContent = item.querySelector(".characteristics__description");
 
-    chButton.addEventListener("click", () => {
-      if (chContent.classList.contains("open")) {
-        chContent.style.height = "";
+    chButton.addEventListener("click", () => { /* указываем обработчик событий вешаем клик взаимодействие */
+      if (chContent.classList.contains("open")) {  /* вешаем открытие  */
+        chContent.style.height = ""; /* прописываем стиль высоты пустой */ /*в стилях прописана высота */
       } else {
-        chContent.style.height = chContent.scrollHeight + "px";
+        chContent.style.height = chContent.scrollHeight + "px"; /* высота опред от элементов */
       }
 
-      chButton.classList.toggle("active");
+      chButton.classList.toggle("active"); /* вешаем кнопки  */
       chContent.classList.toggle("open");
     });
   });
 };
 
-accordeonFunc();
+accordeonFunc(); /* замыкаем функцию */
